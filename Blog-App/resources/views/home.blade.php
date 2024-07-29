896564140b26c63f3ec4bddf44cc3299a57b86ba
Yasanga <yasanga@thesanmark.com>
2024-07-29 11:22:15 +0530

    Add Featured And Latest Posts

---
+++ Blog-App/resources/views/home.blade.php
@@ -0,0 +1,40 @@
+<x-app-layout>
+    @section('hero')
+        <div class="w-full text-center py-32">
+            <h1 class="text-2xl md:text-3xl font-bold text-center lg:text-5xl text-gray-700">
+                Welcome to <span class="text-yellow-500">&lt;YELO&gt;</span> <span class="text-gray-900"> News</span>
+            </h1>
+            <p class="text-gray-500 text-lg mt-1">Best Blog in the universe</p>
+            <a class="px-3 py-2 text-lg text-white bg-gray-800 rounded mt-5 inline-block"
+                href="http://127.0.0.1:8000/blog">Start
+                Reading</a>
+        </div>
+    @endsection
+
+    <div class="w-full mb-10">
+        <div class="mb-16">
+            <h2 class="mt-16 mb-5 text-3xl font-bold text-yellow-500"> Featured Posts </h2>
+            <div class="w-full">
+                <div class="grid w-full grid-cols-3 gap-10">
+                    @foreach ($featuredPosts as $post)
+                        <x-posts.post-card :post="$post" class="col-span-3 md:col-span-1" />
+                    @endforeach
+                </div>
+            </div>
+            <a class="block mt-10 text-lg font-semibold text-center text-yellow-500" href="">
+                {{ __('home.more_posts') }}</a>
+        </div>
+        <hr>
+
+        <h2 class="mt-16 mb-5 text-3xl font-bold text-yellow-500">Latests Posts </h2>
+        <div class="w-full mb-5">
+            <div class="grid w-full grid-cols-3 gap-10">
+                @foreach ($latestPosts as $post)
+                    <x-posts.post-card :post="$post" class="col-span-3 md:col-span-1" />
+                @endforeach
+            </div>
+        </div>
+        <a class="block mt-10 text-lg font-semibold text-center text-yellow-500" href="">
+            {{ __('home.more_posts') }}</a>
+    </div>
+</x-app-layout>
