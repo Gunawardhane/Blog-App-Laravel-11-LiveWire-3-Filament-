c353e6fade62766269abace4582355c995b11cd3
Yasanga <yasanga@thesanmark.com>
2024-07-30 08:19:27 +0530

    Change do in Blog Page

---
+++ Blog-App/app/Livewire/PostList.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Post;
+use Livewire\Attributes\Computed;
+use Livewire\Component;
+
+class PostList extends Component
+{
+
+    #[Computed]
+    public function posts(){
+        return Post::take(5)->get();
+    }
+    public function render()
+    {
+        return view('livewire.post-list');
+    }
+}
