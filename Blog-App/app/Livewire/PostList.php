0936359813b0422ef0a3df29661dedf3f30fb127
Yasanga <yasanga@thesanmark.com>
2024-07-30 09:17:37 +0530

    Add Blog page Pagination & Latest and Oldest

--- Blog-App/app/Livewire/PostList.php
+++ Blog-App/app/Livewire/PostList.php
@@ -4,14 +4,25 @@ namespace App\Livewire;
 
 use App\Models\Post;
 use Livewire\Attributes\Computed;
+use Livewire\Attributes\Url;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class PostList extends Component
 {
+    use WithPagination;
+
+    #[Url()]
+    public $sort = 'desc';
+
+    public function setSort($sort){
+            $this->sort = ($sort === 'desc') ? 'desc' : 'asc';
+            $this->resetPage();
+    }
 
     #[Computed]
     public function posts(){
-        return Post::take(5)->get();
+        return Post::published()->orderBy('published_at', $this->sort)->paginate(3);
     }
     public function render()
     {
