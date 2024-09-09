8893691d7bd445736cd78296ed5bbca1143b9471
Yasanga <yasanga@thesanmark.com>
2024-09-09 11:43:52 +0530

    Added Caching

--- Blog-App/app/Http/Controllers/PostController.php
+++ Blog-App/app/Http/Controllers/PostController.php
@@ -4,15 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Post;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
 
 class PostController extends Controller
 {
     public function index(){
-        return view('posts.index',[
-            'categories' => Category::
+
+
+        $categories = Cache::remember('categories', Carbon::now()->addDay(3), function () {
+            return Category::
             whereHas('posts', function ($query) {
                 $query->published();
-            })->take(10)->get()
+            })->take(10)->get();
+         });
+
+
+
+        return view('posts.index',[
+            'categories' => $categories
         ]);
     }
 
